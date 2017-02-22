037358708e3ffbd4824aa87c5bb5be1ba6b69f49
Alan Tang <zzatang@hotmail.com>
2017-02-23 00:57:51 +1100

    Added filename property and sql_db method

--- atDB.py
+++ atDB.py
@@ -7,4 +7,32 @@ class atDB:
         '''
             db = atDB( table = '', filename = '')
         '''
-        
+        self.filename = kwargs.get('filename')
+        self.table = kwargs.get('table', '')
+
+    def sql_do(self, sql, params = ()):
+        '''
+            sql_do(sql[, params])
+            perform non-select query
+        '''
+        self._db.execute(sql, params)
+        self._db.commit()
+
+
+    @property
+    def filename(self):
+        return self._dbFilename
+
+    @filename.setter
+    def filename(self, fn):
+        self._dbFilename = fn
+        self._db = sqlite3.connect(fn)
+        self._db.row_factory = sqlite3.Row
+
+    @filename.deleter
+    def filename(self):
+        self.close()
+
+    def close(self):
+        self._db.close()
+        del self._dbFilename
