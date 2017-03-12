6f993f765b381aa46ac4f43d21883d9687bb80a8
Alan Tang <zzatang@hotmail.com>
2017-03-12 17:14:33 +1000

    Added lines for atApp.py

--- code_clinic_ex1/atApp.py
+++ code_clinic_ex1/atApp.py
@@ -11,14 +11,38 @@ class atApp:
     
     def __init__ (self, master):
         self.master = master
+        self._createGUI()
         self.database = lpoDB.lpoDB()
 
     
     def _createGUI(self):
+
+        # create style
         bgcolor = '#CCCCFF'
         self.master.configure(background = bgcolor)
         self.master.title("AT Testing")
         self.master.resizable(False, False)
+        self.style = ttk.Style()
+        self.style.configure("TFrame", background = bgcolor)
+        self.style.configure("TButton", background = bgcolor)
+        self.style.configure("TLabel", background = bgcolor)
+        self.style.configure("Status.TLabel", background = bgcolor, font = ("Arial Black", 10))
+        self.style.configure("Result.TLabel", background = bgcolor, font = ("Arial Black", 10))
+
+        # create and display header
+        self.frame_header = ttk.Frame(self.master)
+        self.frame_header.pack(side = TOP)
+        self.logo = PhotoImage(file = "lpo_logo.gif")
+        ttk.Label(self.frame_header, image = self.logo).pack()
+
+        # create and display input 
+        self.frame_input = ttk.Frame(self.master)
+        self.frame_input.pack(side = TOP)
+        
+        ttk.Label(self.frame_input, text = "Start Date:").grid(row = 0, column = 1, columnspan = 3, sticky = "sw")
+
+        
+        
         
 
 
