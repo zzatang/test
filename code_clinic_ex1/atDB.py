4ea752c0f06b53fe91ff9b739e153ec7a48c0b6d
Alan Tang <zzatang@hotmail.com>
2017-03-22 01:19:14 +1100

    Added __init__ method for class atDB

--- code_clinic_ex1/atDB.py
+++ code_clinic_ex1/atDB.py
@@ -1,4 +1,4 @@
-from datetime import date, datetime, timedelta
+﻿from datetime import date, datetime, timedelta
 from tkinter import messagebox
 import sqlite3
 import atWeb
@@ -13,3 +13,43 @@ class atDB():
     def __init__(self, **kwargs):
         self.filename = kwargs.get('filename', 'lpo.db')
         self.table = kwargs.get('tablename', 'Weather')
+        sql = '''CREATE TABLE IF NOT EXISTS {} 
+                (Date Text, Time Text, Status Text, Air_Temp Float, Baromatric_Press Float, Wind_Speed Float)'''.format(self._dbtable)
+        self._db.execute(sql)
+
+
+
+
+
+
+    ### setting filename property
+    @property
+    def filename(self):
+        return self._dbFilename
+
+    @filename.setter
+    def filename(self, fn):
+        self._dbFilename = fn
+        self._db = sqlite3.connect(fn)
+        self._db.row_factory = sqlite3.Row
+
+    @filename.deleter
+    def filename(self):
+        self.close()
+
+    def close(self):
+        self._db.close()
+        del self._dbFilename
+
+    @property
+    def tablename(self):
+        return self._dbTable
+
+    @tablename.setter
+    def tablename(self, t):
+        self._dbTable = t
+
+    @tablename.deleter
+    def tablename(self):
+        del self._dbTable
+
