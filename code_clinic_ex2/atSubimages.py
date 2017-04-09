05d259c40a8cdb7b07e39e4989e6b4733936eec2
Alan Tang <zzatang@hotmail.com>
2017-04-10 00:07:33 +1000

    Completed user interface. Added file search button.

--- code_clinic_ex2/atSubimages.py
+++ code_clinic_ex2/atSubimages.py
@@ -27,10 +27,41 @@ class FindSubset:
         self.path_entry = ttk.Entry(self.main_frame, width = 54)
         self.path_entry.grid(row = 1, column = 0, sticky = 'e')
         self.path_entry.insert(0, '.\\images')
-        self.browse_button = ttk.Button(self.main_frame, text = 'Browse...', command = '')
+        self.browse_button = ttk.Button(self.main_frame, text = 'Browse...', command = self.browse_callback)
         self.browse_button.grid(row = 1, column = 1, sticky = 'w')
-        self.search_button = ttk(self.main_frame, text = 'Find Subset Images', command = '')
+        self.search_button = ttk(self.main_frame, text = 'Find Subset Images', command = self.search_callback)
         self.search_button.grid(row = 2, column = 0, columnspan = 2)
+
+        self.result_table = ttk.Treeview(self.main_frame, column = ('subset'))
+        self.result_table.heading('#0', text = 'Original Image')
+        self.result_table.column('#0', width = 200)
+        self.result_table.heading('subset', text = 'Subset Image')
+        self.result_table.column('subset', width = 200)
+
+        self.status_frame = ttk.Frame(self.master)
+        self.status_frame.pack(fill = BOTH, expand = True)
+
+        self.status_var = StringVar()
+        self.status_label = ttk.Label(self.status_frame, textvariable = self.status_var)
+
+        self.progress_var = DoubleVar()
+        self.progressbar = ttk.Progressbar(self.status_frame, mode = 'determinate', variable = self.progress_var)
+    
+    def browse_callback(self):
+        path = filedialog.askdirectory(initialdir = self.path_entry.get())
+        self.path_entry.delete('0', END)
+        self.path_entry.insert('0', path)
+
+    def search_callback(self):
+        self.start_time = time()
+
+        try:
+            self.path = self.path_entry.get()
+            images = []
+
+
+
+
 
 
 
