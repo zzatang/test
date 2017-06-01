ac136fa201912e764c0641ccb7e21940316a1fc5
Alan Tang <zzatang@hotmail.com>
2017-06-01 23:08:36 +1000

    Added more lines

--- code_clinic_ex4/atTheremin.py
+++ code_clinic_ex4/atTheremin.py
@@ -9,13 +9,13 @@ class atTheremin():
         master.attributes('-fullscreen', True)
         self.canvas = Canvas(master, background = 'black')
         self.canvas.pack(fill = BOTH, expand = True)
-        self.canvas.create_text(self.canvas.winfo_width()//2, self.canvas.winfo_height(), anchor ='s',
+        self.canvas.create_text(master.winfo_screenwidth()//2, master.winfo_screenheight() , anchor ='s',
                                 text = "Press 'Esc' to Quit", font = ('Helvetica', 32, 'bold'), fill = 'white')
 
         master.bind('<Motion>', self.mouse_move)
-        master.bind('<BottonPress>', self.move_down)
-        master.bind('<BottonRelease>', self.mouse_up)
-        master.bind('Escape', lambda e: master.destroy())
+        master.bind('<ButtonPress>', self.mouse_down)
+        master.bind('<ButtonRelease>', self.mouse_up)
+        master.bind('<Escape>', lambda e: master.destroy())
                
 
     def mouse_move(self, event):
@@ -28,10 +28,10 @@ class atTheremin():
             self.prev = event
 
     def mouse_down(self, event):
-        pass
+        self.prev = event
 
     def mouse_up(self, event):
-        pass
+        self.prev = None
 
 
 def main():
