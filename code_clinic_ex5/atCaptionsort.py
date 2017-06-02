7d0a3b697c0e51b4486bc3712d1d72c9817be0fd
Alan Tang <zzatang@hotmail.com>
2017-06-02 22:56:13 +1000

    Added my own py file

---
+++ code_clinic_ex5/atCaptionsort.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+import subprocess
+from tkinter import *
+from tkinter import ttk, filedialog, messagebox
+
+class atCaptionsort():
+    def __init__(self, master):
+        pass
+
+
+def main():
+    root = Tk()
+    gui = atCaptionsort(root)
+    root.mainloop()
+
+if __name__ == '__main__': main()
