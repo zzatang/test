61658ef2df0c85a357e1591571f9d6ba67c37a02
Alan Tang <zzatang@hotmail.com>
2017-08-30 23:08:12 +1000

    Changed url.py to include admin

--- django/Django_myown/Django_myown/urls.py
+++ django/Django_myown/Django_myown/urls.py
@@ -10,8 +10,8 @@ import inventory.forms
 import inventory.views
 
 # Uncomment the next lines to enable the admin:
-# from django.conf.urls import include
-# from django.contrib import admin
+from django.conf.urls import include
+from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
@@ -42,5 +42,5 @@ urlpatterns = [
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
