ec4b3f82d4a885c683e35134dd474c8ccb7fcfdc
Alan Tang <zzatang@hotmail.com>
2017-09-05 15:30:29 +1000

    Added templates and css and js

--- django/Django_myown/inventory/models.py
+++ django/Django_myown/inventory/models.py
@@ -6,6 +6,6 @@ from django.db import models
 
 class Item(models.Model):
     title = models.CharField(max_length = 200)
-    decription = models.TextField()
+    description = models.TextField()
     amount = models.IntegerField()
 
