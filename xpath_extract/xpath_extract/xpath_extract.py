ed4a4e277daa1aa4f8781e12ba9b256fe2d87f1a
Alan Tang <zzatang@hotmail.com>
2018-04-24 04:18:59 +1000

    First complete build

--- xpath_extract/xpath_extract/xpath_extract.py
+++ xpath_extract/xpath_extract/xpath_extract.py
@@ -14,7 +14,8 @@ import csv
 
 def csv_get_site(file_obj, output_path):
     
-    data = ["Category,last_page".split(",")]
+    #data = ["Category,last_page".split(",")]
+    data = [("Category","last_page")]
 
     reader = csv.DictReader(file_obj, delimiter=',')
     for line in reader:
@@ -23,18 +24,19 @@ def csv_get_site(file_obj, output_path):
         tree = html.fromstring(page.content)
         page_count = tree.xpath('count(//div[@class="navigation_block_bottom"]/div[@class="nav-pages"]/a)')
         page_count_final = 1 if page_count == 0 else page_count
-        data.append(",".join((line["category"], str(trunc(page_count_final)))))
+        #data.append(",".join((line["category"], str(trunc(page_count_final)))))
+        data.append((line["category"], trunc(page_count_final)))
         print(",".join((line["category"], str(trunc(page_count_final)))))
-        
-    with open(output_path, "w") as csv_file:
-        writer = csv.writer(csv_file)
+       
+    with open(output_path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',')
         for data_line in data:
             writer.writerow(data_line)
 
 
 
 if __name__=="__main__":
-    old_csv_path = 'C:\\Users\\alan\\Documents\\_Tax returns\\Amazon\\Tactical Arbitrage\\bulk_lists_for_product_search\\budgetgolf_1.csv'
+    old_csv_path = 'C:\\Users\\alan\\Documents\\_Tax returns\\Amazon\\Tactical Arbitrage\\bulk_lists_for_product_search\\budgetgolf.csv'
     new_csv_path = old_csv_path.replace(".csv", "_updated.csv")
     with open(old_csv_path) as f_obj:
         csv_get_site(f_obj, new_csv_path)
