a26981663638f02e0e878e9404534df4609cc740
Alan Tang <zzatang@hotmail.com>
2018-05-05 09:03:21 +1000

    Add write to file

--- xpath_extract/xpath_extract/log_parse.py
+++ xpath_extract/xpath_extract/log_parse.py
@@ -10,7 +10,11 @@ fileObject = open(logFile, "r",encoding='utf-8')
 print("Name of log file: ", fileObject.name)
 #lines = fileObject.readlines()
 lines = list(fileObject)
-#print("Read line: %s" % (lines[2]))
+outputFileName = ''.join(["C:\\Users\\Alan\\Downloads\\rerun",currentDateTime,".csv"])
+oFileObject = open(outputFileName, "w", newline='')
+oWriter = csv.writer(oFileObject)
+oWriter.writerow(["category","spage","lpage","domain"])
+
 rerun = []
 for idx, val in enumerate(lines):
     if (val.strip() == "No products found on this page.") and (idx >= 3):
@@ -21,8 +25,11 @@ for idx, val in enumerate(lines):
         wwwPos = fullMessage.find('www.')
         dotComPos = fullMessage.find('.com')
         domainString = fullMessage[wwwPos+4:dotComPos+4]
-        rerun.append(domainString)
+        rowToWrite = [urlString,"1","1",domainString]
+        oWriter.writerow(rowToWrite)
+        #rerun.append(domainString)
      
        
-print(rerun)
+#print(rerun)
 fileObject.close()
+oFileObject.close()
