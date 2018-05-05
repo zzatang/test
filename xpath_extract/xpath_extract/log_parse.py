c9cc32b8cb62e277056351a7b91382a81df7f2f6
Alan Tang <zzatang@hotmail.com>
2018-05-05 20:25:05 +1000

    Release 1.0

--- xpath_extract/xpath_extract/log_parse.py
+++ xpath_extract/xpath_extract/log_parse.py
@@ -2,34 +2,40 @@
 import csv
 import time
 import datetime
-currentDateTime = datetime.datetime.now().strftime("%Y%m%d_%H%M")
-#open the log file
-logFile = "C:\\Users\\Alan\\Downloads\\last_ps_logs (20).txt"
-#logFile = "c:\\users\\s63914\\downloads\\bikewagon_1_rerun.csv"
-fileObject = open(logFile, "r",encoding='utf-8')
-print("Name of log file: ", fileObject.name)
-#lines = fileObject.readlines()
-lines = list(fileObject)
-outputFileName = ''.join(["C:\\Users\\Alan\\Downloads\\rerun",currentDateTime,".csv"])
-oFileObject = open(outputFileName, "w", newline='')
-oWriter = csv.writer(oFileObject)
-oWriter.writerow(["category","spage","lpage","domain"])
+import os
 
-rerun = []
-for idx, val in enumerate(lines):
-    if (val.strip() == "No products found on this page.") and (idx >= 3):
-        fullMessage = lines[idx-3]
-        firstQuotePos = fullMessage.find('"')
-        secondQuotePos = fullMessage.find('"', firstQuotePos + 1)
-        urlString = fullMessage[firstQuotePos+1:secondQuotePos]
-        wwwPos = fullMessage.find('www.')
-        dotComPos = fullMessage.find('.com')
-        domainString = fullMessage[wwwPos+4:dotComPos+4]
-        rowToWrite = [urlString,"1","1",domainString]
-        oWriter.writerow(rowToWrite)
-        #rerun.append(domainString)
-     
-       
-#print(rerun)
-fileObject.close()
-oFileObject.close()
+
+def CreateRerunFile(inputLogFile, fileCounter):
+    currentDateTime = datetime.datetime.now().strftime("%Y%m%d_%H%M")
+    fileObject = open(inputLogFile, "r",encoding='utf-8')
+    lines = list(fileObject)
+    outputFileName = ''.join(["rerun_",str(fileCounter), "_", currentDateTime,".csv"])
+    oFileObject = open(outputFileName, "w", newline='')
+    oWriter = csv.writer(oFileObject)
+    oWriter.writerow(["category","spage","lpage","domain"])
+
+    for idx, val in enumerate(lines):
+        if (val.strip() == "No products found on this page.") and (idx >= 3):
+            fullMessage = lines[idx-3]
+            firstQuotePos = fullMessage.find('"')
+            secondQuotePos = fullMessage.find('"', firstQuotePos + 1)
+            urlString = fullMessage[firstQuotePos+1:secondQuotePos]
+            wwwPos = fullMessage.find('www.')
+            dotComPos = fullMessage.find('.com')
+            domainString = fullMessage[wwwPos+4:dotComPos+4]
+            rowToWrite = [urlString,"1","1",domainString]
+            oWriter.writerow(rowToWrite)
+
+    fileObject.close()
+    oFileObject.close()
+    os.remove(inputLogFile)
+    if len(rowToWrite) == 0:
+        os.remove(outputFileName)
+
+if __name__ == "__main__":
+    
+    logFileList = [f for f in os.listdir(os.getcwd()) if f.endswith(".txt")]
+
+    for idx, val in enumerate(logFileList):
+        CreateRerunFile(val, idx+1)
+
